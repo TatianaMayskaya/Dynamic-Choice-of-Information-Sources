9c84e035179b2f2eaf3b7ac5c3fbbf23865e0b0e
TatianaMayskaya <43783769+TatianaMayskaya@users.noreply.github.com>
2020-03-13 17:51:58 +0300

    Create plotStrategy.m
    Plot a strategy on the belief space
    Input:
       X, Y: two n x m matrices that correspond to 2-D grid coordinates (all rows of X are identical and correspond to x-axes, all columns of Y are identical and correspond to y-axes)
       points: n x m matrix that correspond to the strategy, each element is either 0 (= no learning), or 1 (= use source 1), or 2 (= use source 2)
       triangle: either True (x-axes is p10, y-axes is p01) or False (x-axes is q1/(1+q1), y-axes is q2/(1+q2)) 
       fig: figure number
    Output:
       fig: next figure number

---
+++ plotStrategy.m
@@ -0,0 +1,23 @@
+function fig = plotStrategy (points, X, Y, triangle, fig)
+% Plot a strategy on the belief space
+% Input:
+%   X, Y: two n x m matrices that correspond to 2-D grid coordinates (all rows of X are identical and correspond to x-axes, all columns of Y are identical and correspond to y-axes)
+%   points: n x m matrix that correspond to the strategy, each element is either 0 (= no learning), or 1 (= use source 1), or 2 (= use source 2)
+%   triangle: either True (x-axes is p10, y-axes is p01) or False (x-axes is q1/(1+q1), y-axes is q2/(1+q2)) 
+%   fig: figure number
+% Output:
+%   fig: next figure number
+figure(fig); fig=fig+1;
+clf; hold on
+source1color = 0.8; % source 1 color is light gray
+source2color = 0.5; % source 2 color is gray
+plot(X(points(:)==1),Y(points(:)==1),'LineStyle','none','Marker','.','Color',[source1color,source1color,source1color])
+plot(X(points(:)==2),Y(points(:)==2),'LineStyle','none','Marker','.','Color',[source2color,source2color,source2color])
+xlim([0,1]); ylim([0,1]); xticks([]); yticks([])
+if triangle
+  box off
+  plot([0,1],[1,0],'LineWidth',2,'Color','k')
+else
+  box on
+end
+end
