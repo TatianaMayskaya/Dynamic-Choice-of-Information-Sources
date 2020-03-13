162a0d366412968b6a624f263c17f132ed3c2dff
TatianaMayskaya <tmayskaya@gmail.com>
2020-03-13 20:05:42 +0300

    Update ind.m

--- ind.m
+++ ind.m
@@ -1,10 +1,10 @@
 function row = ind (theta1, theta2)
 % Map (theta1,theta2) to the row number in payoff matrix
-% Input: 
+% Input:
 %   theta1, theta2: state (00, 01, 10, or 11)
 % Output:
-%   row: 1, 2, 3 or 4 
-if theta1+theta2==0
+%   row: 1, 2, 3 or 4
+if theta1 + theta2==0
     row=1;
 elseif (theta2==1)&&(theta1==0)
     row=2;
@@ -13,4 +13,4 @@ elseif (theta1==1)&&(theta2==0)
 else
     row=4;
 end
-end
+end 
