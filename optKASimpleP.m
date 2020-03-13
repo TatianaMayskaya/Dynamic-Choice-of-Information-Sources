00ac6c033bc60a47919f89c4664e7dec22bd35d1
TatianaMayskaya <tmayskaya@gmail.com>
2020-03-13 20:20:48 +0300

    Update optKASimpleP.m

--- optKASimpleP.m
+++ optKASimpleP.m
@@ -8,14 +8,14 @@ function [points, EV] = optKASimpleP (u, k, a, P10, P01)
 % Output:
 %   points: n x m matrix that corresponds to the optimal (k,a)-strategy, each element is either 0 (= no learning), or k (= use source k)
 %   EV: n x m matrix that corresponds to the expected value from the optimal (k,a)-strategy
-if k = 1
+if k == 1
   delta_u = max(u(ind(1,0),:)) - u(ind(1,0),a);
   p = P10; 
 else
   delta_u = max(u(ind(0,1),:)) - u(ind(0,1),a);
   p = P01;
 end
-cont_region = p*delta_u>=1;
+cont_region = (p*delta_u>=1) && (P10+P01<=1);
 points = zeros(size(p)); points(cont_region) = k;
 EV = P10*u(ind(1,0),a) + P01*u(ind(0,1),a) + (1-P10-P01)*u(ind(0,0),a);
 EV(cont_region) = EV(cont_region) + p(cont_region)*delta_u - 1 - (1-p(cont_region)).*log((delta_u-1)./(1./p(cont_region)-1));
